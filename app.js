285a5687b70a9987e45ca6ac968e893d8c19dc1f
Nayan <nayanchoubey.nc@gmail.com>
2021-09-26 22:59:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Lets see what happens'));
+app.get('/', (req, res) => res.send('Got it'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
